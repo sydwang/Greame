477b3144a9261e731b740d1cfc5f13a9e9cf45c7
wangyijun <wangyijun@localhost>
2016-10-28 15:45:42 +1100

    append a line of output.

---
+++ testgood.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Run on Build-in Webserver</title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        echo 'A successful test!';
+        echo '<br/>';
+        echo 'A perfect closure!';
+        ?>
+    </body>
+</html>
